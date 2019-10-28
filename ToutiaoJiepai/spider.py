9f32b5d76cfa1f66e4c904947c287ab5391e8201
涛 <997933096@qq.com>
2019-10-28 16:54:39 +0800

    头条结束

--- ToutiaoJiepai/spider.py
+++ ToutiaoJiepai/spider.py
@@ -1,8 +1,20 @@
-import requests
-import time
-from urllib.parse import urlencode
-from requests.exceptions import RequestException
+import json
+import os
 import random
+import re
+import time
+import pymongo
+import requests
+from hashlib import md5
+from urllib.parse import urlencode
+from bs4 import BeautifulSoup
+from requests.exceptions import RequestException
+from json.decoder import JSONDecodeError
+from multiprocessing import Pool
+from config import *
+
+client = pymongo.MongoClient(MONGO_URL, connect=False)
+db = client[MONGO_DB]
 
 
 def get_page_index(offset, keyword):
@@ -14,31 +26,34 @@ def get_page_index(offset, keyword):
         'keyword': keyword,
         'autoload': 'true',
         'count': '20',
-        'en_qc': 1,
         'cur_tab': 3,
+        'en_qc': 1,
         'from': 'media',
         'pd': 'user',
         'timestamp': time.time()
     }
     url = 'http://www.toutiao.com/api/search/content/?' + urlencode(data)
+    print(url)
     headers = {
-        'Accept': 'application/json, text/javascript',
-        'Accept-Encoding': 'gzip, deflate, sdch',
-        'Accept-Language': 'zh-CN,zh;q=0.8',
+        'Accept':
+        'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'zh-CN,zh;q=0.9',
         'x-requested-with': 'XMLHttpRequest',
         'Content-Type': 'application/x-www-form-urlencoded',
         'User-Agent':
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36'
+        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36',
+        'Referer': 'https://www.toutiao.com/'
     }
     timeout = random.choice(range(80, 180))
     cookies = {
-        'tt_webid': '6752410193911563783',
-        'WEATHER_CITY': '北京',
-        's_v_web_id': '19c27c191458ee7f6233602c35635966',
-        'csrftoken': '63ce749e21d2a8f6031ede0fffe4c0a0'
+        'tt_webid': '6752653947729790467',
     }
     try:
-        response = requests.get(url,  headers=headers, timeout=timeout)
+        response = requests.get(url,
+                                headers=headers,
+                                cookies=cookies,
+                                timeout=timeout)
         if response.status_code == 200:
             return response.text
         return None
@@ -47,10 +62,88 @@ def get_page_index(offset, keyword):
         return None
 
 
-def main():
-    html = get_page_index(0, '街拍')
-    print(html)
+def parse_page_index(html):
+    try:
+        data = json.loads(html)
+        if data and 'data' in data.keys():
+            for item in data.get('data'):
+                yield item.get('article_url')
+    except JSONDecodeError:
+        pass
+
+
+# 请求详情页的组图
+def get_page_detail(url):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.text
+        return None
+    except RequestException:
+        print('请求详情页出错', url)
+        return None
+
+
+# 解析url的详情内容
+def parse_page_detail(html, url):
+    soup = BeautifulSoup(html, 'lxml')
+    title = soup.select('title')[0].get_text()
+    print(title)
+    images_pattern = re.compile('var gallery = (.*?);', re.S)
+    result = re.search(images_pattern, html)
+    if result:
+        data = json.loads(result.group(1))
+        if data and 'sub_images' in data.keys():
+            sub_images = data.get('sub_images')
+            images = [item.get('url') for item in sub_images]
+            for image in images:
+                download_image(image)
+            return {'title': title, 'url': url, 'images': images}
+
+
+# 存到数据库
+def save_to_mongo(result):
+    if db[MONGO_TABLE].insert(result):
+        print("存储到MongoDB成功", result)
+        return True
+    return False
+
+
+# 下载图片
+def download_image(url):
+    print('正在下载', url)
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            save_image(response.content)
+        return None
+    except RequestException:
+        print('请求图片出错', url)
+        return None
+
+
+# 保存到本地
+def save_image(content):
+    file_path = '{0}/{1}.{2}'.format(os.getcwd(),
+                                     md5(content).hexdigest(), 'jpg')
+    if not os.path.exists(file_path):
+        with open(file_path, 'wb') as f:
+            f.write(content)
+            f.close()
+
+
+def main(offset):
+    html = get_page_index(offset, KEYWORD)
+    for url in parse_page_index(html):
+        html = get_page_detail(url)
+        if html:
+            result = parse_page_detail(html, url)
+            if result:
+                save_to_mongo(result)
 
 
 if __name__ == "__main__":
     main()
+    groups = [x * 20 for x in range(GROUP_START, GROUP_END + 1)]
+    pool = Pool()
+    pool.map(main, groups)
